bb3226e7cd9ab657e86ae2480470e948877f29d7
Alan <alan_biju@outlook.com>
2021-08-07 18:12:55 +0530

    ✨ Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# fancy fonts generator
- A simple web app that can convert user input into unicode pseudofonts.
+# 💬 𝕱𝖆𝖓𝖈𝖞 𝕱𝖔𝖓𝖙𝖘 𝕲𝖊𝖓𝖊𝖗𝖆𝖙𝖔𝖗
+
+A super-simple web app that you can use to generate cool, weird, and beautiful text using pseudofonts made with Unicode characters. This is the GUI-based equivalent of the [Fancy Fonts Bot](https://github.com/waterrmalann/telegram-fancy-fonts-bot) for Telegram that I wrote not too long ago. They are both similar in terms of functionality and the font system is cross-compatible.
+
+**Note:** Text generated using this application may not always be readable, they could also cause accessibility issues as screen-readers might not recognize them. So it is advised that you use them knowing the downsides.*
+
+> 𝕿𝖍𝖎𝖘 𝖕𝖆𝖗𝖆𝖌𝖗𝖆𝖕𝖍 𝖔𝖋 𝖙𝖊𝖝𝖙 𝖜𝖆𝖘 𝖌𝖊𝖓𝖊𝖗𝖆𝖙𝖊𝖉 𝖚𝖘𝖎𝖓𝖌 𝖙𝖍𝖊 𝖜𝖊𝖇 𝖆𝖕𝖕.
+> 𝓘𝓽 𝓾𝓼𝓮𝓼 𝓼𝓸𝓶𝓮 𝓸𝓯 𝓽𝓱𝓮 𝓭𝓲𝓯𝓯𝓮𝓻𝓮𝓷𝓽 𝓯𝓸𝓷𝓽𝓼 𝓽𝓱𝓪𝓽 𝓪𝓻𝓮 𝓪𝓿𝓪𝓲𝓵𝓪𝓫𝓵𝓮 𝓫𝔂 𝓭𝓮𝓯𝓪𝓾𝓵𝓽.
+> 𝕋𝕙𝕚𝕤 𝕜𝕚𝕟𝕕 𝕠𝕗 𝕥𝕖𝕩𝕥 𝕔𝕒𝕟 𝕓𝕖 𝕔𝕠𝕡𝕚𝕖𝕕 𝕒𝕟𝕕 𝕡𝕒𝕤𝕥𝕖𝕕 𝕒𝕟𝕪𝕨𝕙𝕖𝕣𝕖.
+> 𝑻𝒉𝒆𝒚 𝒘𝒊𝒍𝒍 𝒔𝒕𝒊𝒍𝒍 𝒓𝒆𝒕𝒂𝒊𝒏 𝒕𝒉𝒆𝒊𝒓 𝒐𝒓𝒊𝒈𝒊𝒏𝒂𝒍 𝒍𝒐𝒐𝒌 𝒂𝒔 𝒍𝒐𝒏𝒈 𝒂𝒔 𝒕𝒉𝒆 𝒑𝒍𝒂𝒕𝒇𝒐𝒓𝒎 𝒔𝒖𝒑𝒑𝒐𝒓𝒕𝒔 𝒖𝒏𝒊𝒄𝒐𝒅𝒆.
+> 𝚃𝚑𝚎𝚢 𝚖𝚒𝚐𝚑𝚝 𝚜𝚘𝚖𝚎𝚝𝚒𝚖𝚎𝚜 𝚋𝚎 𝚍𝚒𝚏𝚏𝚒𝚌𝚞𝚕𝚝 𝚝𝚘 𝚛𝚎𝚊𝚍 𝚑𝚘𝚠𝚎𝚟𝚎𝚛, 𝚊𝚗𝚍 𝚌𝚊𝚞𝚜𝚎 𝚊𝚌𝚌𝚎𝚜𝚜𝚒𝚋𝚒𝚕𝚒𝚝𝚢 𝚒𝚜𝚜𝚞𝚎𝚜.
+> Ｓｏ Ｉ'ｄ ｒｅｃｏｍｍｅｎｄ ｎｏｔ ｕｓｉｎｇ ｉｔ ｆｏｒ ｉｍｐｏｒｔａｎｔ ｔｅｘｔ.
+> 🅞🅝🅛🅨 🅤🅢🅔 🅘🅣 🅣🅞 🅗🅐🅥🅔 🅕🅤🅝!
+
+#### Features
+
+- Real-time text to pseudofont conversion.
+- 27 unique fonts pre-defined. Easy to add more thanks to the simple system.
+- Can double as a pun generator. 😛
+- Ability to convert all available fonts at the same time.
+
+#### To-Do
+
+- More fonts.
+- Improve the UI and make it look better.
+- Make the psuedofont class more advanced to support custom symbols and replacements with more than 1 character. (eg: "V" to "\\/")
+
+## 🚀 Getting Started
+
+### Live Version
+
+I have a live version of the web app up on my [personal portfolio](http://alanvarghese.me/projects/fancy-fonts-generator/app.html). It is always on the exact same version as this git repository. If you however want to host your own version of the app, follow the steps down below to get started.
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/en/) (for the package manager)
+- Live Server (`npm install -g live-server`, run with `live-server`)
+
+### Hosting locally
+
+1. [Clone the repository](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository-from-github/cloning-a-repository).
+```
+git clone https://github.com/waterrmalann/telegram-fancy-fonts-bot.git
+```
+2. Install the dependencies. (not required yet)
+```
+npm install
+```
+3. Start a live server. Make sure you are in the app directory.
+```
+live-server
+```
+
+### Adding new fonts
+
+The fun stuff! Fonts are stored in a json file in the root directory with the name `fonts.json`. Each font is stored in this format:
+
+```json
+{
+    "fontName": "FullWidth",
+    "fontLower": "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ",
+    "fontUpper": "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ",
+    "fontDigits": "０１２３４５６７８９"
+}
+```
+
+Leave an empty string for empty parameters. It will default to english letters and numbers.
+
+- **fontName**: The name of the font.
+- **fontUpper**: Uppercase letters of that font.
+- **fontLower**: Lowercase letters of that font.
+- **fontDigits**: Numbers of that font.
+
+Note: **fontLower** and **fontUpper** must always be 26 characters. **fontDigits** must always be 10 characters. The pseudofont class currently isn't failsafe. Foolproofing it is on my todo list.
+
+## 🤝 Contributing
+
+Contributions are accepted and there really isn't any strict rules. Feel free to open a pull request to fix any issues or to make improvements you think that should be made. You can also add new fonts to the font database or help me with the to-do list above. Any contribution will be accepted as long as it doesn't stray too much from the objective of the app. If you're in doubt about whether the PR would be accepted or not, you can always open an issue to get my opinion on it.
+
+License
+----
+
+MIT License, see [LICENSE](LICENSE)
---
+++ js/converter.js
@@ -0,0 +1,246 @@
+/*********************************
+* 
+* Fancy Fonts Generator/Converter 
+* Written by Alan.
+* Written on 06-08-2021
+*
+/*********************************/
+
+/* String Extensions */
+String.prototype.unicodeAwareSplit = function() {
+	// it was a nightmare to figure out that javascript doesn't handle surrogate pairs properly
+	// im a python programmer :/
+	let _arr = [];
+	for (const _c of this.valueOf()) {
+		_arr.push(_c);
+	}
+	return _arr;
+}
+
+String.prototype.toAlternateCase = function() {
+	let _arr = [];
+	let _alternate = true;
+	for (const _c of this.valueOf()) {
+		if (_alternate) {
+			_alternate = false;
+			_arr.push(_c.toUpperCase());
+		} else {
+			_alternate = true;
+			_arr.push(_c.toLowerCase());
+		}
+	}
+	return _arr.join('');
+};
+
+/* PseudoFont: Unicode Font Parser & Converter */
+class PseudoFont {
+	constructor(fontName, fontLower, fontUpper, fontDigits) {
+		this.fontName = fontName;
+		
+		// splitting because otherwise JavaScript won't handle the characters properly.
+		this.fontLower = fontLower.unicodeAwareSplit();
+		this.fontUpper = fontUpper.unicodeAwareSplit();
+		this.fontDigits = fontDigits.unicodeAwareSplit();
+		
+		this.referenceLower = "abcdefghijklmnopqrstuvwxyz";
+		this.referenceUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		this.referenceDigits = "0123456789";
+	}
+	
+	convert(rawText) {
+		let _converted = "";
+		for (const _char of rawText) {
+			if (this.referenceLower.includes(_char)) {
+				// if character is lowercase
+				_converted += this.fontLower[this.referenceLower.indexOf(_char)];
+			} else if (this.referenceUpper.includes(_char)) {
+				// if character is uppercase
+				_converted += this.fontUpper[this.referenceUpper.indexOf(_char)];
+			} else if (this.referenceDigits.includes(_char)) {
+				// if character is digit
+				_converted += this.fontDigits[this.referenceDigits.indexOf(_char)];
+			} else {
+				_converted += _char;
+			}
+		}
+		return _converted;
+	}
+}
+
+/* Variables */
+const randomTexts = [
+	"Why did Adele cross the road? To say hello from the other side.", "What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.",
+	"What did the grape say when it got crushed? Nothing, it just let out a little wine.", "I want to be cremated as it is my last hope for a smoking hot body.",
+	"Time flies like an arrow. Fruit flies like a banana.", "To the guy who invented zero, thanks for nothing.",
+	"I had a crazy dream last night! I was swimming in an ocean of orange soda. Turns out it was just a Fanta sea.",
+	"A crazy wife says to her husband that moose are falling from the sky. The husband says, it’s reindeer.",
+	"Ladies, if he can’t appreciate your fruit jokes, you need to let that mango.",
+	"Geology rocks but Geography is where it’s at!", "I had a crazy dream last night! I was swimming in an ocean of orange soda. Turns out it was just a Fanta sea.",
+	"A crazy wife says to her husband that moose are falling from the sky. The husband says, it’s reindeer.",
+	"Ladies, if he can’t appreciate your fruit jokes, you need to let that mango.",
+	"Geology rocks but Geography is where it’s at!", "Smaller babies may be delivered by stork but the heavier ones need a crane.",
+	"My grandpa has the heart of the lion and a lifetime ban from the zoo.", "Why was Dumbo sad? He felt irrelephant.",
+	"A man sued an airline company after it lost his luggage. Sadly, he lost his case.", "I lost my mood ring and I don’t know how to feel about it!",
+	"Yesterday, I accidentally swallowed some food coloring. The doctor says I’m okay, but I feel like I’ve dyed a little inside.",
+	"So what if I don’t know what apocalypse means? It’s not the end of the world!",
+	"My friend drove his expensive car into a tree and found out how his Mercedes bends.", "Becoming a vegetarian is one big missed steak.",
+	"I was wondering why the ball was getting bigger. Then it hit me.", "Some aquatic mammals at the zoo escaped. It was otter chaos!",
+	"Never trust an atom, they make up everything!", "Waking up this morning was an eye-opening experience.",
+	"Long fairy tales have a tendency to dragon.", "What do you use to cut a Roman Emperor’s hair? Ceasers."
+];
+
+var convertAll = false;  // whether to convert and display all available fonts or not.
+var randomText = "The quick brown fox jumps over the lazy dog.";  // random text used for placeholder if user input is null.
+var userInput = ""; // input from the user (updated on keyup)
+var selectedFont = "";  // the font the user selected.
+var selectedStyle = "";  // the font style the user selected.
+var fonts = [];   // all the fonts that the user can choose from
+
+/* Elements */
+const e_inputTextArea = document.getElementById('input-text-area');
+const e_fontSelect = document.getElementById('font-select');
+const e_fontStyleSelect = document.getElementById('font-style-select');
+const e_viewAllConversions = document.getElementById('view-all-conversions');
+
+const e_outputText = document.getElementById('output-text');
+const e_outputList = document.getElementById('output-list');
+
+// Fetch the fonts.json file and set everything up.
+// To-Do: add a fallback in case the fonts can't be fetched (in case of running offline or something)
+fetch("fonts.json")
+.then(response => response.json())
+.then(_fontFiles => {
+	for (const _font of _fontFiles) {
+		// make a new pseudofont object.
+		let _newFont = new PseudoFont(
+			_font['fontName'],
+			_font['fontLower'],
+			_font['fontUpper'],
+			_font['fontLower']
+		);
+		
+		// add the font to the font list.
+		fonts.push(_newFont);
+		
+		// add the font to the fonts selection list.
+		let _newFontOption = document.createElement('option');
+		_newFontOption.value = _newFont.fontName;
+		_newFontOption.innerHTML = `${_newFont.fontName} (${_newFont.convert(_newFont.fontName)})`;
+		e_fontSelect.appendChild(_newFontOption);
+	}
+	
+	// enable the text area once the fonts have been loaded.
+	e_inputTextArea.disabled = false;
+	
+	// show how many fonts are loaded inside parenthesis
+	e_viewAllConversions.innerText = `View All Fonts (${fonts.length})`;
+	
+	// select a random font to show at start.
+	e_fontSelect.selectedIndex = Math.floor(Math.random() * fonts.length);
+	
+	// update all variables and convert the first text.
+	updateUserInput();
+	updateFontInput();
+	convertText();
+});
+
+/* Event Listeners */
+// Convert text on update of the text field.
+e_inputTextArea.addEventListener('keyup', () => {
+	updateUserInput(); 
+	convertTextAll();
+});
+
+// Convert text when font is changed from the list.
+e_fontSelect.addEventListener('change', () => {
+	updateFontInput();
+	convertText();
+});
+
+// Convert text when font style is changed from the list.
+e_fontStyleSelect.addEventListener('change', () => {
+	updateFontInput();
+	updateUserInput();
+	convertText();
+});
+
+// Enable/disable whether to convert all fonts or not.
+e_viewAllConversions.addEventListener('click', () => {
+	convertAll = (convertAll ? false : true);
+	convertTextAll();
+});
+
+// Copy content to clipboard if the user clicks on the converted text.
+e_outputText.addEventListener('click', () => {
+	let _range = document.createRange();
+	window.getSelection().removeAllRanges();
+	_range.selectNode(e_outputText);
+	window.getSelection().addRange(_range);
+	document.execCommand('copy');
+	window.getSelection().removeAllRanges();
+	
+	// To-Do: replace this with a less annoying pop-up later.
+	alert("Copied to clipboard!");
+});
+
+/* Update Functions */
+function updateUserInput() {
+	let _userInput = e_inputTextArea.value;
+	if (_userInput.trim()) {
+		userInput = _userInput;
+	} else {
+		// generate a new random placeholder if the textarea has no value.
+		updateRandomText();
+		userInput = randomText;
+	}
+	
+	switch (selectedStyle) {
+		case "shift-upper":
+			userInput = userInput.toUpperCase();
+			break;
+		case "shift-lower":
+			userInput = userInput.toLowerCase();
+			break;
+		case "shift-alternate":
+			userInput = userInput.toAlternateCase();;
+			break;
+		case "spaced":
+			userInput = userInput.split('').join(' ');
+			break;
+	}
+}
+
+function updateFontInput() {
+	// update the selected font and its style.
+	selectedFont = fonts.find(fnt => fnt.fontName === e_fontSelect.value);
+	selectedStyle = e_fontStyleSelect.value;
+}
+
+function updateRandomText() {
+	// new random pun for placeholder.
+	randomText = randomTexts[Math.floor(Math.random() * randomTexts.length)];
+}
+
+/* Conversion Functions */
+function convertText() {
+	// update the main font output.
+	e_outputText.innerHTML = selectedFont.convert(userInput);
+}
+
+function convertTextAll() {
+	// update the main font output.
+	convertText();
+	
+	// make sure we convert everything only if the user has clicked the "View All" <detail> tag.
+	if (convertAll) {
+		// remove all children of the output list
+		e_outputList.innerHTML = "";
+		
+		// convert the text and display all available fonts.
+		for (const _font of fonts) {
+			let _li = document.createElement("li");
+			_li.innerHTML = `<p> <b class="unselectable">${_font.fontName}:</b> ${_font.convert(userInput)}</p>`;
+			e_outputList.appendChild(_li);
+		}
+	}
+}
